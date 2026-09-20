f87ab6b5f4c929ff01d87aeda5f602d03c8d7dfe
aryvitorss <ary99.souza@gmail.com>
2026-09-20 09:13:47 -0300

    ajuste videosmock e sobre

--- src/app/videos-mock.ts
+++ src/app/videos-mock.ts
@@ -7,7 +7,7 @@ export const VIDEOS: Video[] = [
     title: "CENA 1 - A VIDA DE JÔ",
     thumbnail: "https://img.youtube.com/vi/wV8XGFPM8z0/sddefault.jpg",
     youtubeURL: "https://www.youtube.com/embed/wV8XGFPM8z0",
-    creator: "Aryzinho - Ary Vitor Dos Anjos",
+    creator: "Ary Vitor Dos Anjos",
     likes: 0,
     views: 0
   },
@@ -16,7 +16,7 @@ export const VIDEOS: Video[] = [
     title: "CENA 2 - O PECADO E MAL",
     thumbnail:"https://img.youtube.com/vi/oK_M18mX1eU/sddefault.jpg",
     youtubeURL: "https://www.youtube.com/embed/oK_M18mX1eU",
-    creator: "Aryzinho - Ary Vitor Dos Anjos",
+    creator: "Ary Vitor Dos Anjos",
     likes: 0,
     views: 0
   },
@@ -25,7 +25,7 @@ export const VIDEOS: Video[] = [
     title: "CENA 3 - O SOFRIMENTO DE JÔ",
     thumbnail:"https://img.youtube.com/vi/pPBdYQR1PkQ/sddefault.jpg",
     youtubeURL: "https://www.youtube.com/embed/pPBdYQR1PkQ",
-    creator: "Aryzinho - Ary Vitor Dos Anjos",
+    creator: "Ary Vitor Dos Anjos",
     likes: 0,
     views: 0
   },
@@ -34,7 +34,7 @@ export const VIDEOS: Video[] = [
     title: "CENA 4 - OS DONS DE DEUS",
     thumbnail:"https://img.youtube.com/vi/rswzOLBHTCw/sddefault.jpg",
     youtubeURL:"https://www.youtube.com/embed/rswzOLBHTCw",
-    creator: "Aryzinho - Ary Vitor Dos Anjos",
+    creator: "Ary Vitor Dos Anjos",
     likes: 0,
     views: 0
   },
@@ -43,7 +43,7 @@ export const VIDEOS: Video[] = [
     title: "CENA 5 - JÔ E UMA VIDA NOVA",
     thumbnail: "https://img.youtube.com/vi/2VwQUKDDCUU/sddefault.jpg",
     youtubeURL: "https://www.youtube.com/embed/2VwQUKDDCUU",
-    creator: "Aryzinho - Ary Vitor Dos Anjos",
+    creator: "Ary Vitor Dos Anjos",
     likes: 0,
     views: 0
   }
